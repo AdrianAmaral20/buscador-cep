bba45a8e198f04cd35426fa75376d2cbc437b5d0
Adrian Amaral <adri13amaral@gmail.com>
2023-03-12 09:29:04 -0300

    estrutura basica de html e container estilizado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { FiSearch } from 'react-icons/fi';
+import './App.css';
+
 function App() {
   return (
-    <div className="App">
-      <h1>teste</h1>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input type="text" placeholder="Digite seu CEP..." />
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#fff" />
+        </button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: </h2>
+        <span>Rua </span>
+        <span>Complemento: alugmgasjd</span>
+        <span>Vila Rosa</span>
+        <span>Campo grande - ms</span>
+      </main>
     </div>
   );
 }
